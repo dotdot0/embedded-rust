2f11fafe546a94fec308aabcd95f3fb2467ccfe5
Pratush Rai <pratushrai0309@gmail.com>
2022-10-23 01:45:13 -0400

    Buttons Support

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused, unused_comparisons)]
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
@@ -10,41 +11,68 @@ use panic_rtt_target as _;
 use microbit::{
     board::Board,
     display::blocking::Display,
-    hal::{prelude::*, Timer}
+    hal::{prelude::*, Timer, gpio}
 };
-
-const PIXEL: [(usize, usize); 16] = [
-    (0,0), (0,1), (0,2), (0,3), (0,4), (2,4), (1,4), (3,4), (0,4),
-    (4,3), (4,2), (4,1), (4,0), (3,0), (2,0), (1,0)
-];
-
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
 
+    //let button_a = board.buttons.button_b.is_low().unwrap();
+
+    let mut ch_pos: (usize, usize) = (4, 0);
+
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-    let mut leds = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
+    let mut leds_a = [
+        [0, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0],
     ];
-    let mut second_last = (1, 1);
-    let mut last_led = (0, 0);
+    let mut leds_b = [
+        [1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 0, 0],
+    ];
+
+    let mut character =[ 
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+    ];
     // infinite loop; just so we don't leave this stack frame
     loop {
-        for current_led in PIXEL.iter(){
-            leds[second_last.0][second_last.1] = 0;
-            leds[last_led.0][last_led.1] = 1;
-            leds[current_led.0][current_led.1] = 1;
-            display.show(&mut timer, leds, 500);
-            rprintln!("{:?}, {:?}", current_led, last_led);
-            second_last = last_led;
-            last_led = *current_led;
+        display.show(&mut timer, character, 200);
+        if let Ok(true) = board.buttons.button_b.is_low(){
+            if ch_pos.1 <= 4{
+                character[ch_pos.0][ch_pos.1] = 0;
+                ch_pos.1 += 1;
+                character[ch_pos.0][ch_pos.1] = 1;
+                display.show(&mut timer, character, 200);
+                rprintln!("Pos: {:?}", ch_pos)
+            }
+            else {
+                ch_pos.1 = 0;
+                display.show(&mut timer, character, 200)
+            }
         }
+        else if let Ok(true) = board.buttons.button_a.is_low() {
+            //display.show(&mut timer, character, 200);
+            if ch_pos.1 >= 0{
+                character[ch_pos.0][ch_pos.1] = 0;
+                ch_pos.1 -= 1;
+                character[ch_pos.0][ch_pos.1] = 1;
+                display.show(&mut timer, character, 200);
+                rprintln!("Pos: {:?}", ch_pos)
+            }
+        }
+        //timer.delay_ms(200u16)
     }
 }
