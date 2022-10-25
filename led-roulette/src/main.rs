85d01419371ff2fa82948ce25bf3992b43585c5d
Pratush Rai <pratushrai0309@gmail.com>
2022-10-25 00:51:35 -0400

    I2C

--- led-roulette/src/main.rs
+++ led-roulette/src/main.rs
@@ -19,9 +19,9 @@ fn main() -> ! {
     rtt_init_print!();
     let mut board = Board::take().unwrap();
 
-    //let button_a = board.buttons.button_b.is_low().unwrap();
+    // let button_a = board.buttons.;
 
-    let mut ch_pos: (usize, usize) = (4, 0);
+    let mut ch_pos: [(usize, usize); 2] = [(4, 0), (3, 0)];
 
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
@@ -44,31 +44,37 @@ fn main() -> ! {
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
         [1, 0, 0, 0, 0],
     ];
     // infinite loop; just so we don't leave this stack frame
     loop {
         display.show(&mut timer, character, 200);
         if let Ok(true) = board.buttons.button_b.is_low(){
-            if ch_pos.1 <= 4{
-                character[ch_pos.0][ch_pos.1] = 0;
-                ch_pos.1 += 1;
-                character[ch_pos.0][ch_pos.1] = 1;
+            if ch_pos[0].1 <= 4 && ch_pos[1].1 <= 4{
+                character[ch_pos[0].0][ch_pos[0].1] = 0;
+                character[ch_pos[1].0][ch_pos[1].1] = 0;
+                ch_pos[0].1 += 1;
+                ch_pos[1].1 += 1;
+                character[ch_pos[0].0][ch_pos[0].1] = 1;
+                character[ch_pos[1].0][ch_pos[1].1] = 1;
                 display.show(&mut timer, character, 200);
                 rprintln!("Pos: {:?}", ch_pos)
             }
-            else {
-                ch_pos.1 = 0;
-                display.show(&mut timer, character, 200)
-            }
+            // else {
+            //     ch_pos.1 = 0;
+            //     display.show(&mut timer, character, 200)
+            // }
         }
         else if let Ok(true) = board.buttons.button_a.is_low() {
             //display.show(&mut timer, character, 200);
-            if ch_pos.1 >= 0{
-                character[ch_pos.0][ch_pos.1] = 0;
-                ch_pos.1 -= 1;
-                character[ch_pos.0][ch_pos.1] = 1;
+            if ch_pos[0].1 >= 0 && ch_pos[1].0 >= 0{
+                character[ch_pos[0].0][ch_pos[0].1] = 0;
+                character[ch_pos[1].0][ch_pos[1].1] = 0;
+                ch_pos[0].1 -= 1;
+                ch_pos[1].1 -= 1;
+                character[ch_pos[0].0][ch_pos[1].1] = 1;
+                character[ch_pos[1].0][ch_pos[1].1] = 1;
                 display.show(&mut timer, character, 200);
                 rprintln!("Pos: {:?}", ch_pos)
             }
